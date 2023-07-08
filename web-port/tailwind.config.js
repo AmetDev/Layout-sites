f4ab82f8e9f08a4e9e7f966851b69d454d9a1a98
AmetJunior <ametsejdaliev@gmail.com>
2023-07-08 19:30:34 +0300

    web-portofolio-component-Header-desktop-done

---
+++ web-port/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        midblack: '#0F1624',
+      },
+    },
+  },
+  plugins: [],
+}
+
