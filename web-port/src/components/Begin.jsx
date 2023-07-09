2381c6bd42232e4913e7cf9dd0b2679f61673fc4
AmetJunior <ametsejdaliev@gmail.com>
2023-07-09 15:55:50 +0300

    notebook

--- web-port/src/components/Begin.jsx
+++ web-port/src/components/Begin.jsx
@@ -7,7 +7,7 @@ export function Begin() {
             <div>
                 <p className="text-xl font-semibold ml-[92px] max-w-screen-sm mt-[52px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             </div>
-            <button className="ml-[139px] w-[250px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-xl">Let's begin</button>
+            <button className="transition ease-in-out delay-300 hover:-translate-y-1 hover:scale-110 hover:transition-colors delay-300 hover:bg-gradient-to-l from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300 ml-[139px] w-[250px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-xl hover:transition-colors">Let's begin</button>
         </div>
     )
 }
