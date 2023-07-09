3be9b6d2395a56fe0a5f4499f75c6423f3f8480d
AmetJunior <ametsejdaliev@gmail.com>
2023-07-09 18:08:39 +0300

    fgdfg

--- web-port/src/components/Begin.jsx
+++ web-port/src/components/Begin.jsx
@@ -5,9 +5,130 @@ export function Begin() {
                 <h1 className="text-white font-semibold text-6xl ml-[32px]">Lorem ipsum dolor sit amet</h1>
             </div>
             <div>
-                <p className="text-xl font-semibold ml-[92px] max-w-screen-sm mt-[52px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                <p className="text-xl text-[#BCBCBC] font-semibold ml-[92px] max-w-screen-sm mt-[52px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             </div>
-            <button className="ml-[139px] w-[250px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-xl">Let's begin</button>
-        </div>
+            <button className="ml-[139px] w-[220px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold mt-[65px] text-xl">Let's begin</button>
+            <div className="position: absolute flex justify-end">
+                <svg width="719" height="760" viewBox="0 0 719 760" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g opacity="0.5">
+                <path d="M671.539 496.615L584.077 409.154" stroke="#505685"/>
+                <path d="M759 409.154L671.539 321.692" stroke="url(#paint0_linear_336_10)"/>
+                <path d="M584.077 409.154L671.539 321.692" stroke="#505685"/>
+                <path d="M671.539 496.615L759 409.154" stroke="#505685"/>
+                <path d="M584.077 409.154L467.462 292.538" stroke="#505685"/>
+                <path d="M671.539 321.692L554.923 205.077" stroke="url(#paint1_linear_336_10)"/>
+                <path d="M467.462 292.538L554.923 205.077" stroke="#505685"/>
+                <path d="M584.077 409.154L671.539 321.692" stroke="url(#paint2_linear_336_10)"/>
+                <path d="M350.846 1L438.308 88.4615" stroke="#505685"/>
+                <path d="M263.385 88.4615L350.846 175.923" stroke="#505685"/>
+                <path d="M438.308 88.4615L350.846 175.923" stroke="#505685"/>
+                <path d="M350.846 1L263.385 88.4615" stroke="#505685"/>
+                <path d="M438.308 88.4615L554.923 205.077" stroke="url(#paint3_linear_336_10)"/>
+                <path d="M350.846 175.923L467.462 292.538" stroke="#505685"/>
+                <path d="M438.308 88.4615L350.846 175.923" stroke="#505685"/>
+                <path d="M467.462 292.538L554.923 380" stroke="#505685"/>
+                <path d="M380 380L467.462 467.461" stroke="#505685"/>
+                <path d="M554.923 380L467.462 467.461" stroke="#505685"/>
+                <path d="M554.923 380L671.539 496.615" stroke="#505685"/>
+                <path d="M467.462 467.461L584.077 584.077" stroke="#505685"/>
+                <path d="M671.539 496.615L584.077 584.077" stroke="url(#paint4_linear_336_10)"/>
+                <path d="M554.923 380L467.462 467.461" stroke="#505685"/>
+                <path d="M380 380L292.539 292.538" stroke="#505685"/>
+                <path d="M467.462 292.538L380 205.077" stroke="#505685"/>
+                <path d="M292.539 292.538L380 205.077" stroke="#505685"/>
+                <path d="M292.539 292.538L175.924 175.923" stroke="#505685"/>
+                <path d="M380 205.077L263.385 88.4615" stroke="#505685"/>
+                <path d="M175.923 175.923L263.385 88.4615" stroke="url(#paint5_linear_336_10)"/>
+                <path d="M292.539 292.538L380 205.077" stroke="#505685"/>
+                <path d="M496.615 671.539L409.154 584.077" stroke="#505685"/>
+                <path d="M584.077 584.077L496.615 496.615" stroke="#505685"/>
+                <path d="M409.154 584.077L496.615 496.615" stroke="#505685"/>
+                <path d="M496.615 671.539L584.077 584.077" stroke="#505685"/>
+                <path d="M409.154 584.077L292.538 467.462" stroke="#505685"/>
+                <path d="M496.615 496.615L380 380" stroke="#505685"/>
+                <path d="M292.538 467.462L380 380" stroke="#505685"/>
+                <path d="M409.154 584.077L496.615 496.615" stroke="url(#paint6_linear_336_10)"/>
+                <path d="M175.923 175.923L263.385 263.385" stroke="#505685"/>
+                <path d="M88.4615 263.385L175.923 350.846" stroke="#505685"/>
+                <path d="M263.385 263.385L175.923 350.846" stroke="#505685"/>
+                <path d="M175.923 175.923L88.4614 263.385" stroke="#505685"/>
+                <path d="M263.385 263.385L380 380" stroke="url(#paint7_linear_336_10)"/>
+                <path d="M175.923 350.846L292.539 467.462" stroke="#505685"/>
+                <path d="M263.385 263.385L175.923 350.846" stroke="#505685"/>
+                <path d="M292.538 467.462L380 554.923" stroke="#505685"/>
+                <path d="M205.077 554.923L292.538 642.385" stroke="#505685"/>
+                <path d="M380 554.923L292.538 642.385" stroke="#505685"/>
+                <path d="M380 554.923L496.615 671.539" stroke="url(#paint8_linear_336_10)"/>
+                <path d="M292.538 642.385L409.154 759" stroke="url(#paint9_linear_336_10)"/>
+                <path d="M496.615 671.538L409.154 759" stroke="#505685"/>
+                <path d="M380 554.923L292.538 642.385" stroke="#505685"/>
+                <path d="M205.077 554.923L117.615 467.462" stroke="#505685"/>
+                <path d="M292.539 467.462L205.077 380" stroke="#505685"/>
+                <path d="M117.615 467.462L205.077 380" stroke="#505685"/>
+                <path d="M117.615 467.462L1.00008 350.846" stroke="#505685"/>
+                <path d="M205.077 380L88.4616 263.385" stroke="#505685"/>
+                <path d="M1.00006 350.846L88.4616 263.385" stroke="#505685"/>
+                <path d="M117.615 467.462L205.077 380" stroke="url(#paint10_linear_336_10)"/>
+                </g>
+                <defs>
+                <linearGradient id="paint0_linear_336_10" x1="759" y1="386.636" x2="699.059" y2="347.071" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="1" stop-color="#FF6B00"/>
+                </linearGradient>
+                <linearGradient id="paint1_linear_336_10" x1="670.044" y1="321.692" x2="530.554" y2="264.135" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint2_linear_336_10" x1="606.595" y1="409.154" x2="646.16" y2="349.212" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="1" stop-color="#FF6B00"/>
+                </linearGradient>
+                <linearGradient id="paint3_linear_336_10" x1="438.308" y1="118.486" x2="518.23" y2="171.239" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.421875" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint4_linear_336_10" x1="649.021" y1="496.615" x2="609.456" y2="556.557" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.0001" stop-color="#505685"/>
+                <stop offset="0.317708" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint5_linear_336_10" x1="175.923" y1="175.923" x2="218.689" y2="70.3552" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.494792" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint6_linear_336_10" x1="431.672" y1="584.077" x2="471.237" y2="524.135" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FF6B00"/>
+                <stop offset="0.234375" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint7_linear_336_10" x1="292.912" y1="293.286" x2="395.908" y2="335.272" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.75" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint8_linear_336_10" x1="380" y1="584.947" x2="459.922" y2="637.701" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.0001" stop-color="#505685"/>
+                <stop offset="0.317708" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint9_linear_336_10" x1="292.538" y1="672.409" x2="372.461" y2="725.162" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.0001" stop-color="#505685"/>
+                <stop offset="0.317708" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                <linearGradient id="paint10_linear_336_10" x1="117.615" y1="467.462" x2="160.381" y2="361.894" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#505685"/>
+                <stop offset="0.0582928" stop-color="#FF6B00"/>
+                <stop offset="1" stop-color="#505685"/>
+                </linearGradient>
+                </defs>
+                </svg>
+
+</div>
+</div>
     )
 }
