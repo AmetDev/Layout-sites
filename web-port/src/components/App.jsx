fdb7bb33990996bc5e5e3e2eb664aeb554d73390
AmetJunior <ametsejdaliev@gmail.com>
2023-07-09 21:38:52 +0300

    projects-beta-add

--- web-port/src/components/App.jsx
+++ web-port/src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Header } from "./Header";
 import { Begin } from "./Begin";
-
+import { Projects } from "./Projects";
 
 function App() {
   return (
     <div className="max-w-screen-xl mx-auto">
       <Header/>
-  
       <Begin />
+      <Projects/>
     </div>
     
 
---
+++ web-port/src/components/Projects.jsx
@@ -0,0 +1,22 @@
+export function Projects() {
+    return (
+        <div className="">
+            <h1 className="text-white font-semibold text-5xl mt-[260px] mb-[70px]">Projects</h1>
+            <div className="flex"> 
+                <div className="inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg w-[250px] h-[250px] mb-5 mr-5">
+                    <img src="" alt="" />
+                    <p className="underline">PROJECT 1</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
+                    <button>click</button>
+                </div>
+                <div className="inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg w-[250px] h-[250px] mb-5 mr-5">
+                    <img src="" alt="" />
+                    <p className="underline">PROJECT 1</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
+                    <button>click</button>
+                </div>
+            </div>
+        </div>
+    )
+
+}
