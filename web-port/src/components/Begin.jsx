f34d6d72d7e5d79e907b7838025058d6e698488e
AmetJunior <ametsejdaliev@gmail.com>
2023-07-10 17:44:11 +0300

    projects-add

--- web-port/src/components/Begin.jsx
+++ web-port/src/components/Begin.jsx
@@ -1,12 +1,13 @@
 export function Begin() {
     return (
-        <div className="mt-[230px] bg-slate-500">
+        <div className="mt-[230px]">
             <div className="inline-block w-[730px] ">
                 <div className="">
                     <h1 className="text-white font-semibold text-4xl ml-[32px]">Lorem ipsum dolor sit amet</h1>
                     <div>
-                    <p className="text-[18px] text-[#BCBCBC] font-semibold ml-[92px] max-w-screen-sm mt-[52px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    <button className="transition ease-in-out delay-300 hover:-translate-y-1 hover:scale-110 hover:transition-colors delay-300 hover:bg-gradient-to-l from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300 ml-[139px] w-[250px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-xl hover:transition-colors">Let's begin</button>
+                    <p className="text-[36x] text-[#BCBCBC] font-semibold ml-[92px] max-w-screen-sm mt-[52px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <button className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300 ml-[139px] w-[250px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-xl hover:transition-colors">Let's begin</button>
+
                 </div>
                 </div>    
             </div>
--- web-port/src/components/Projects.jsx
+++ web-port/src/components/Projects.jsx
@@ -1,22 +1,171 @@
 export function Projects() {
     return (
-        <div className="">
-            <h1 className="text-white font-semibold text-5xl mt-[260px] mb-[70px]">Projects</h1>
-            <div className="flex"> 
-                <div className="inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg w-[250px] h-[250px] mb-5 mr-5">
-                    <img src="" alt="" />
-                    <p className="underline">PROJECT 1</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
-                    <button>click</button>
+        <div>
+            <h1 className="text-white font-semibold text-3xl mt-11 ml-16">Projects</h1>
+            <div className="flex flex-wrap justify-center mt-12">
+            
+            <div className="basis-1/3 mt-8 ml-8 mr-8 flex justify-center inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg mb-5 mr-5 p-[25px] shadow-lg  shadow-indigo-500/40">
+                    <div className="flex justify-center flex-col"> 
+                        <div className="inline-block   w-[300px]">
+                            <div className=" flex justify-center items-center flex-col">
+                            <div className=" bg-slate-50 inline-block w-[300px] h-[266px] flex justify-center items-center">
+                                <svg width="349" height="238" viewBox="0 0 49 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.9002 29.8056L24.5002 0L44.1002 29.8056H4.9002ZM18.0075 24.0611L24.4091 13.1841L30.8108 24.0611H18.0075Z" fill="url(#paint0_linear_313_6)"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M49 37.513L24.5 0L0 37.513H49ZM34.7894 29.2756L24.614 14.697L14.4386 29.2756H34.7894Z" fill="url(#paint1_linear_313_6)"/>
+                                <defs>
+                                <linearGradient id="paint0_linear_313_6" x1="32.8047" y1="9.98228" x2="1.29181" y2="29.9805" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#6233C6"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                <linearGradient id="paint1_linear_313_6" x1="14.1193" y1="12.5636" x2="53.6649" y2="37.4881" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#A27AFA"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                </defs>
+                                </svg>
+                            </div>
+                        
+                            <p className="text-slate-400 font-semibold text-xl">PROJECT 1</p>
+                            <svg width="300" height="4" viewBox="0 0 300 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="300" height="4" rx="2" fill="url(#paint0_linear_3_257)"/>
+                            <defs>
+                            <linearGradient id="paint0_linear_3_257" x1="300" y1="2" x2="1.05302e-06" y2="2.00016" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#13ADC7"/>
+                            <stop offset="0.666667" stop-color="#6978D1"/>
+                            <stop offset="1" stop-color="#945DD6"/>
+                            </linearGradient>
+                            </defs>
+                            </svg>
+                            </div> 
+                            <p className="inline-block text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
+                            <button className="ml-0 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300  w-[180px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-md hover:transition-colors">Look it up</button>
+                    
                 </div>
-                <div className="inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg w-[250px] h-[250px] mb-5 mr-5">
-                    <img src="" alt="" />
-                    <p className="underline">PROJECT 1</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
-                    <button>click</button>
+                
+            </div>
+            </div>
+            <div className="basis-1/3 mt-8 ml-8 mr-8 flex justify-center inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg mb-5 mr-5 p-[25px] shadow-lg  shadow-indigo-500/40">
+                    <div className="flex justify-center flex-col"> 
+                        <div className="inline-block  w-[300px]">
+                            <div className=" flex justify-center items-center flex-col">
+                            <div className=" bg-slate-50 inline-block w-[300px] h-[266px] flex justify-center items-center">
+                                <svg width="349" height="238" viewBox="0 0 49 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.9002 29.8056L24.5002 0L44.1002 29.8056H4.9002ZM18.0075 24.0611L24.4091 13.1841L30.8108 24.0611H18.0075Z" fill="url(#paint0_linear_313_6)"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M49 37.513L24.5 0L0 37.513H49ZM34.7894 29.2756L24.614 14.697L14.4386 29.2756H34.7894Z" fill="url(#paint1_linear_313_6)"/>
+                                <defs>
+                                <linearGradient id="paint0_linear_313_6" x1="32.8047" y1="9.98228" x2="1.29181" y2="29.9805" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#6233C6"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                <linearGradient id="paint1_linear_313_6" x1="14.1193" y1="12.5636" x2="53.6649" y2="37.4881" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#A27AFA"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                </defs>
+                                </svg>
+                            </div>
+                        
+                            <p className="text-slate-400 font-semibold text-xl">PROJECT 1</p>
+                            <svg width="300" height="4" viewBox="0 0 300 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="300" height="4" rx="2" fill="url(#paint0_linear_3_257)"/>
+                            <defs>
+                            <linearGradient id="paint0_linear_3_257" x1="300" y1="2" x2="1.05302e-06" y2="2.00016" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#13ADC7"/>
+                            <stop offset="0.666667" stop-color="#6978D1"/>
+                            <stop offset="1" stop-color="#945DD6"/>
+                            </linearGradient>
+                            </defs>
+                            </svg>
+                            </div> 
+                            <p className="inline-block text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
+                            <button className="ml-0 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300  w-[180px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-md hover:transition-colors">Look it up</button>
+                    
                 </div>
+                
+            </div>
+            </div>
+            <div className="basis-1/3 mt-8 ml-8 mr-8 flex justify-center  inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg mb-5 mr-5 p-[25px] shadow-lg  shadow-indigo-500/40">
+                    <div className="flex justify-center flex-col"> 
+                        <div className="inline-block  w-[300px]">
+                            <div className=" flex justify-center items-center flex-col">
+                            <div className=" bg-slate-50 inline-block w-[300px] h-[266px] flex justify-center items-center">
+                                <svg width="349" height="238" viewBox="0 0 49 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.9002 29.8056L24.5002 0L44.1002 29.8056H4.9002ZM18.0075 24.0611L24.4091 13.1841L30.8108 24.0611H18.0075Z" fill="url(#paint0_linear_313_6)"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M49 37.513L24.5 0L0 37.513H49ZM34.7894 29.2756L24.614 14.697L14.4386 29.2756H34.7894Z" fill="url(#paint1_linear_313_6)"/>
+                                <defs>
+                                <linearGradient id="paint0_linear_313_6" x1="32.8047" y1="9.98228" x2="1.29181" y2="29.9805" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#6233C6"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                <linearGradient id="paint1_linear_313_6" x1="14.1193" y1="12.5636" x2="53.6649" y2="37.4881" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#A27AFA"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                </defs>
+                                </svg>
+                            </div>
+                        
+                            <p className="text-slate-400 font-semibold text-xl">PROJECT 1</p>
+                            <svg width="300" height="4" viewBox="0 0 300 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="300" height="4" rx="2" fill="url(#paint0_linear_3_257)"/>
+                            <defs>
+                            <linearGradient id="paint0_linear_3_257" x1="300" y1="2" x2="1.05302e-06" y2="2.00016" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#13ADC7"/>
+                            <stop offset="0.666667" stop-color="#6978D1"/>
+                            <stop offset="1" stop-color="#945DD6"/>
+                            </linearGradient>
+                            </defs>
+                            </svg>
+                            </div> 
+                            <p className="inline-block text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
+                            <button className="ml-0 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300  w-[180px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-md hover:transition-colors">Look it up</button>
+                    
+                </div>
+                
+            </div>
+            </div>
+            <div className="basis-1/3 mt-8 ml-8 mr-8 flex justify-center inline-block border rounded rounded-br-lg border-indigo-500/100 rounded-tl-lg mb-5 mr-5 p-[25px] shadow-lg  shadow-indigo-500/40">
+                    <div className="flex justify-center flex-col"> 
+                        <div className="inline-block  w-[300px]">
+                            <div className=" flex justify-center items-center flex-col">
+                            <div className=" bg-slate-50 inline-block w-[300px] h-[266px] flex justify-center items-center">
+                                <svg width="349" height="238" viewBox="0 0 49 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.9002 29.8056L24.5002 0L44.1002 29.8056H4.9002ZM18.0075 24.0611L24.4091 13.1841L30.8108 24.0611H18.0075Z" fill="url(#paint0_linear_313_6)"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M49 37.513L24.5 0L0 37.513H49ZM34.7894 29.2756L24.614 14.697L14.4386 29.2756H34.7894Z" fill="url(#paint1_linear_313_6)"/>
+                                <defs>
+                                <linearGradient id="paint0_linear_313_6" x1="32.8047" y1="9.98228" x2="1.29181" y2="29.9805" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#6233C6"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                <linearGradient id="paint1_linear_313_6" x1="14.1193" y1="12.5636" x2="53.6649" y2="37.4881" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#A27AFA"/>
+                                <stop offset="1" stop-color="#76EFFF"/>
+                                </linearGradient>
+                                </defs>
+                                </svg>
+                            </div>
+                        
+                            <p className="text-slate-400 font-semibold text-xl">PROJECT 1</p>
+                            <svg width="300" height="4" viewBox="0 0 300 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="300" height="4" rx="2" fill="url(#paint0_linear_3_257)"/>
+                            <defs>
+                            <linearGradient id="paint0_linear_3_257" x1="300" y1="2" x2="1.05302e-06" y2="2.00016" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#13ADC7"/>
+                            <stop offset="0.666667" stop-color="#6978D1"/>
+                            <stop offset="1" stop-color="#945DD6"/>
+                            </linearGradient>
+                            </defs>
+                            </svg>
+                            </div> 
+                            <p className="inline-block text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iste ipsum quod porro nam dolore harum nisi placeat quos eli</p>
+                            <button className="ml-0 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300  w-[180px] h-[60px] bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold m-4 text-md hover:transition-colors">Look it up</button>
+                    
+                </div>
+                
+            </div>
             </div>
         </div>
+        </div>
+      
     )
-
 }
