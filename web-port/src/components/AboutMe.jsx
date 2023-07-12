71027f7f9159e5b31edba727e37734a10a116285
AmetJunior <ametsejdaliev@gmail.com>
2023-07-12 20:55:26 +0300

    fixed-techonologies-1

--- web-port/src/components/AboutMe.jsx
+++ web-port/src/components/AboutMe.jsx
@@ -1,43 +1,46 @@
 export function AboutMe() {
     return (
-        <div className="mt-[300px] mb-[420px]">
-            <p className="text-white font-semibold text-[64px]">About me</p>
-            <div>
-                <div className="mt-[80px] w-[1289px] h-[12px] bg-slate-50 inline-block flex items-center rounded-full justify-between">
-                    <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
-                        <div className="text-white mb-6 font-semibold text-[36px] ">2020</div>
-                        <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
-                            <p className="text-center">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
+        <div className="mb-[420px] inline-block">
+                <div className="mt-[300px] mb-[420px]">
+                <p className="text-white font-semibold text-[64px]">About me</p>
+                <div>
+                    <div className="mt-[80px] w-[1289px] h-[12px] bg-slate-50 inline-block flex items-center rounded-full justify-between">
+                        <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
+                            <div className="text-white mb-6 font-semibold text-[36px] ">2020</div>
+                            <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
+                                <p className="text-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
                         </div>
+                        <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
+                            <div className="text-white mb-6 font-semibold text-[36px] ">2021</div>
+                            <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
+                                <p className="text-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
+                            <div className="text-white mb-6 font-semibold text-[36px] ">2022</div>
+                            <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
+                                <p className="text-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
+                            <div className="text-white mb-6 font-semibold text-[36px] ">2023</div>
+                            <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
+                                <p className="text-center">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
+                        </div>   
                     </div>
-                    <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
-                        <div className="text-white mb-6 font-semibold text-[36px] ">2021</div>
-                        <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
-                            <p className="text-center">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
-                        <div className="text-white mb-6 font-semibold text-[36px] ">2022</div>
-                        <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
-                            <p className="text-center">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="rounded-full w-[32px] h-[32px] bg-slate-50 justify-center flex items-center flex-col">
-                        <div className="text-white mb-6 font-semibold text-[36px] ">2023</div>
-                        <div className="inline-block w-[347px] h-[48px] mt-4 font-semibold text-[28px] text-white">
-                            <p className="text-center">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </div>
-                    </div>   
                 </div>
             </div>
         </div>
+        
     )
 }
