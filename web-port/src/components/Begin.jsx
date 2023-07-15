11ce1c1b45cbcf2a286bf43462d2c7f3ef473b6f
Dash <dasha.semivolos@gmail.com>
2023-07-15 22:25:36 +0300

    adaptation and add text

--- web-port/src/components/Begin.jsx
+++ web-port/src/components/Begin.jsx
@@ -1,12 +1,14 @@
 export function Begin() {
     return (
         // max-2xl:w-600px
-        <div className="mb-[268px] max-2xl:w-600px"> 
+        <div className="mb-[268px]"> 
             <div className="inline-block w-[1030px]">
-                <div className="">
-                    <h1 className="text-white font-semibold text-[64px] ml-[32px] max-md:text-[44px] max-lg:w-[600px] ">Lorem ipsum dolor sit amet</h1>
-                    <div className="max-md:w-50px">
-                    <p className="text-[36px] text-[#BCBCBC] font-semibold ml-[92px] w-[1057px] mt-[55px] text-start max-md:text-[26px] max-lg:w-[580px] max-lg:ml-[32px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                <div>
+                    <h1 className="text-white font-semibold text-[64px] ml-[32px] 
+                    max-2xl:text-[44px] max-lg:ml-[44px] max-2xl:ml-[92px]">Who is AmexDev?</h1>
+                    <div>
+                    <p className="text-[36px] text-[#BCBCBC] font-semibold ml-[92px] w-[1057px] mt-[55px] text-start max-2xl:text-[24px] 
+                    max-lg:w-[700px] max-xl:w-[800px] max-lg:ml-[44px]">I'm a JavaScript Developer with experience in creating beautiful and functional websites. I am proficient in various web development technologies. My stack: HTML, CSS, JavaScript, React, NodeJs, Nginx, Linux: Ubuntu/Fedora.</p>
                     <button className="ml-[92px] mt-[80px] transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none from-midindigo from-10% via-midcyan via-30% to-midblue to-90% duration-300  w-[280px] h-[80px] 
                     bg-gradient-to-r from-midindigo from-10% via-midcyan via-30% to-midblue to-90% text-white  rounded-full font-semibold text-[28px] hover:transition-colors">Let's begin</button>
                     </div>
