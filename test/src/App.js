68fd329b324bd3b8c76dd01347a86dee6946f150
AmetDev <ametsejdaliev@gmail.com>
2023-07-17 22:53:13 +0300

    not-done

---
+++ test/src/App.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+export function App() {
+  const name = "Amex";
+  return (
+    <div className="">
+      <h1 className="text-4xl text-teal-400 inline-block bg-gray-400 h-100 w-100 w-full">
+        {name}
+      </h1>
+    </div>
+  );
+}
