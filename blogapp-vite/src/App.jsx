1ca1418dce03c0aad2e870d4539811a04ee390b2
AmetDev <ametsejdaliev@gmail.com>
2023-07-20 17:44:42 +0300

    add-Profile-Header

--- blogapp-vite/src/App.jsx
+++ blogapp-vite/src/App.jsx
@@ -1,9 +1,11 @@
-import Menu from "./Menu.jsx";
+import Menu from "./components/Menu.jsx";
+import Profile from "./components/Profile.jsx";
 function App() {
   return (
-    <>
+    <div className="flex justify-between flex-col w-max-[1200px] ml-[20%] mr-[20%]">
       <Menu />
-    </>
+      <Profile />
+    </div>
   );
 }
 
---
+++ blogapp-vite/src/components/Profile.jsx
@@ -0,0 +1,15 @@
+function Profile() {
+  return (
+    <div className=" flex font-medium  items-center ">
+      <div className="bg-teal-300 h-16 w-16 rounded-full"></div>
+      <div className="flex flex-col ml-4 ">
+        <div className="text-teal-300  font-bold flex ">
+          <p className="text-white ">FrontEnd Dev&nbsp;</p>
+          AmexDev
+        </div>
+        <p className="text-white">We have to try every day to be better.</p>
+      </div>
+    </div>
+  );
+}
+export default Profile;
